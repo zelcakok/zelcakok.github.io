bafd329d13ee1fc36cff025627faf2f5fb4b184e
KokTszHoZelca <zelcakok@gmail.com>
2019-03-19 12:59:23 +0800

    Test

---
+++ precache-manifest.2d08946bdc52728f426f25936873c42d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d2b4725a2da3e9627eb7",
+    "url": "/static/js/main.d2b4725a.chunk.js"
+  },
+  {
+    "revision": "568eccfaaa1d3bfe3e38",
+    "url": "/static/js/2.568eccfa.chunk.js"
+  },
+  {
+    "revision": "d2b4725a2da3e9627eb7",
+    "url": "/static/css/main.18b05b20.chunk.css"
+  },
+  {
+    "revision": "9553fe78d40c201babd79b3706630818",
+    "url": "/index.html"
+  }
+];
